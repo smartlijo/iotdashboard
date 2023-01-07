0ef0952373a755507365073dbe2476904b7af1c8
Aymen Frikha <aymen.frikha@lijo.com>
2023-01-07 04:26:18 +0000

    fix telegraf and influxdb setup

--- infrastructure/influxdb/setup-influxdb.sh
+++ infrastructure/influxdb/setup-influxdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 microk8s.helm3 repo add influxdata https://helm.influxdata.com/
-microk8s.helm3 upgrade -f values.yaml --install iotdb influxdata/influxdb2 --namespace=monitoring
+microk8s.helm3 install -f values.yaml  --namespace=monitoring iotdb influxdata/influxdb2
 echo $(kubectl get secret iotdb-influxdb2-auth -o "jsonpath={.data['admin-password']}" --namespace monitoring | base64 --decode)
