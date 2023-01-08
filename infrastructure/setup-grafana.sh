392fe51fcc8402c32235f507c3c559eb32dd2bc3
Aymen Frikha <aymen.frikha@lijo.com>
2023-01-08 18:10:15 +0000

    grafana automation with Jenkins

--- infrastructure/setup-grafana.sh
+++ infrastructure/setup-grafana.sh
@@ -2,7 +2,4 @@
 microk8s.kubectl create secret generic grafana-creds -n monitoring \
   --from-literal=GF_SECURITY_ADMIN_USER=admin \
   --from-literal=GF_SECURITY_ADMIN_PASSWORD=admin1234
-
-microk8s.kubectl apply -f grafana.yaml
-
-
+kubectl apply -f ./grafana/grafana.yaml
